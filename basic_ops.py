4ada733f82b517dd3fb52b7ab2cca7d7d6a968aa
Toufic Arabi <tarabi@tarabi-a01.vmware.com>
2020-08-28 12:45:07 -0400

    additonal method

--- basic_ops.py
+++ basic_ops.py
@@ -124,3 +124,20 @@ num = 10
 for num in range(5):
     print(num)
 print(num)
+
+print ("================")
+print ("Vowels + Constants counter for string below")
+school = 'Massachusetts Institute of Technology'
+numVowels = 0
+numCons = 0
+
+for char in school:
+    if char == 'a' or char == 'e' or char == 'i' \
+       or char == 'o' or char == 'u':
+        numVowels += 1
+    elif char == 'o' or char == 'M':
+        print(char)
+    else:
+        numCons -= 1
+print('numVowels is: ' + str(numVowels))
+print('numCons is: ' + str(numCons)) 
