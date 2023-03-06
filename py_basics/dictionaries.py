b93a1b9cb4a8d1432bb2094e91cb73b4ee612a50
Toufic Arabi <toufic.arabi@gmail.com>
2023-03-06 14:19:09 -0500

    dict with f string

--- py_basics/dictionaries.py
+++ py_basics/dictionaries.py
@@ -77,7 +77,25 @@ def biggest(aDict):
 
 print(biggest(animals))
         
-        
+#some cool formatting with string below        
+dictionary = {
+    "0" : "Zero",
+    "1" : "One",
+    "2" : "Two",
+    "3" : "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine",
+    "10": "Ten"
+    } #key value pairs, keys need to be unique
 
+phone_number = input ("Phone:")
+numbers_to_words = ""
+for character in phone_number:
+    numbers_to_words = f'{numbers_to_words}{dictionary.get(character)} '
+print (numbers_to_words)
 
 
